a5a6622d4151ba2335d9e41a0ae6221a2ddf5e0f
yousufii <idris.yousuf94@gmail.com>
2021-11-07 06:53:48 +0000

    Functions excercise

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function () {
+  console.log("Hello, world");
+}
+sayHello();
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
